761c83728dbfb3503869dc9af71291d3d14af1f2
geniuxy <2238384910@qq.com>
2026-07-06 17:36:41 +0800

    【摄像机】结合LookAt的定义，自己实现LookAt矩阵的计算

--- camera.h
+++ camera.h
@@ -63,6 +63,7 @@ public:
     glm::mat4 GetViewMatrix()
     {
         return glm::lookAt(Position, Position + Front, Up);
+        // return calculate_lookAt_matrix(Position, Position + Front, Up);
     }
 
     // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
@@ -121,6 +122,37 @@ private:
         // also re-calculate the Right and Up vector
         Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
         Up    = glm::normalize(glm::cross(Right, Front));
+    }
+
+    glm::mat4 calculate_lookAt_matrix(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
+    {
+        // 1. Position = known
+        // 2. Calculate cameraDirection
+        glm::vec3 zaxis = glm::normalize(position - target);
+        // 3. Get positive right axis vector
+        glm::vec3 xaxis = glm::normalize(glm::cross(glm::normalize(worldUp), zaxis));
+        // 4. Calculate camera up vector
+        glm::vec3 yaxis = glm::cross(zaxis, xaxis);
+
+        // Create translation and rotation matrix
+        // 由于采用列优先存储方式，我们通过mat[col][row]来访问元素
+        glm::mat4 translation = glm::mat4(1.0f); // Identity matrix by default
+        translation[3][0] = -position.x; // Third column, first row
+        translation[3][1] = -position.y;
+        translation[3][2] = -position.z;
+        glm::mat4 rotation = glm::mat4(1.0f);
+        rotation[0][0] = xaxis.x; // First column, first row
+        rotation[1][0] = xaxis.y;
+        rotation[2][0] = xaxis.z;
+        rotation[0][1] = yaxis.x; // First column, second row
+        rotation[1][1] = yaxis.y;
+        rotation[2][1] = yaxis.z;
+        rotation[0][2] = zaxis.x; // First column, third row
+        rotation[1][2] = zaxis.y;
+        rotation[2][2] = zaxis.z; 
+
+        // Return lookAt matrix as combination of translation and rotation matrix
+        return rotation * translation; // Remember to read from right to left (first translation then rotation)
     }
 };
 #endif
